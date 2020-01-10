2074621fd74efed5af245f5be48e7f7dcf0495a1
Valerie <valerietimmerman2000@gmail.com>
2020-01-10 15:45:45 +0100

    Set up start for application.

---
+++ src/app/models/HttpMethods.ts
@@ -0,0 +1,6 @@
+export const enum HttpMethods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+}
---
+++ src/app/models/ServerModel.ts
@@ -0,0 +1,12 @@
+export class ServerModel {
+
+  static host = 'localhost';
+  static port = '8080';
+
+  constructor() {
+
+  }
+
+
+}
+
